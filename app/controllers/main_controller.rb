caaf2997f439c80888e87b39c7177aba3ee33a67
Andrey Seleznov <andreii.seleznov@gmail.com>
2015-03-23 19:40:49 +0200

    main. view data

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
   def index
     @disclosures = FinanceDisclosure.all
 
-    @max_general_income = 1
-    @max_general_income = (@disclosures.map {|disclosure| disclosure.general_income_value}).max if @disclosures.present?
+    @max_general_income = (@disclosures.map {|disclosure| disclosure.general_income_value}).max
+    @max_general_income = 1 if @max_general_income == 0
   end
 end
